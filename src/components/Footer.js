70ee5b07c2bc2727db320d458a2c03938762601b
tmdgus95 <8571053@naver.com>
2023-02-22 12:31:08 +0900

    포트폴리오

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="inner">
+        <p>Copyright ⓒ Park Sieun All Right Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Vision.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Vision = () => {
+  return (
+    <section className="vision scroll">
+      <div className="inner">
+        {/* 1년 목표, 3년후 목표, 5년후 목표 */}
+        <h2>Vision</h2>
+        <div className="contents">
+          <ul className="vision-list">
+            <li>1년후 :</li>
+            <li>3년후 :</li>
+            <li>5년후 :</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Vision;
